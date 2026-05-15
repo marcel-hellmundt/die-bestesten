275650d9c5f9bbb66230711ed9451de18b57ca4b
marcel-hellmundt <github@marcel-hellmundt.de>
2026-05-15 14:07:25 +0200

    Remove Statistiken from Liga nav and delete liga-stats component

--- webapp/src/app/liga/liga.module.ts
+++ webapp/src/app/liga/liga.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LigaComponent } from './liga.component';
 import { MatchdayComponent } from './matchday/matchday.component';
 import { TableComponent } from './table/table.component';
-import { LigaStatsComponent } from './stats/liga-stats.component';
 import { HallOfFameComponent } from '../hall-of-fame/hall-of-fame.component';
 
 const routes: Routes = [
@@ -15,14 +14,13 @@ const routes: Routes = [
       { path: '',             redirectTo: 'spieltag', pathMatch: 'full' },
       { path: 'spieltag',      component: MatchdayComponent },
       { path: 'tabelle',       component: TableComponent },
-      { path: 'statistiken',   component: LigaStatsComponent },
       { path: 'ruhmeshalle', component: HallOfFameComponent },
     ]
   }
 ];
 
 @NgModule({
-  declarations: [LigaComponent, MatchdayComponent, TableComponent, LigaStatsComponent, HallOfFameComponent],
+  declarations: [LigaComponent, MatchdayComponent, TableComponent, HallOfFameComponent],
   imports: [CommonModule, RouterModule.forChild(routes)]
 })
 export class LigaModule {}
